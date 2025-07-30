c7b502d1914fa802399cf1461f4fcb5ed79915ec
Junaid Atari <junaid.attari@invozone.dev>
2025-07-30 14:30:15 +0500

    feat: create repositories classes

---
+++ Database/Repositories/ProductImageRepository.cs
@@ -0,0 +1,8 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository: https://github.com/blacksmoke26/ims-backend
+
+namespace Database.Repositories;
+
+public sealed class ProductImageRepository(ApplicationDbContext context) : RepositoryBase<ProductImage>(context) {
+}
