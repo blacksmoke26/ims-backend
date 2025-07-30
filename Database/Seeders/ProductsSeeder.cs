b59f4ddb36f31a0919451cbc7549aa346698a8dc
Junaid Atari <junaid.attari@invozone.dev>
2025-07-30 12:31:44 +0500

    feat: add seeder classes

---
+++ Database/Seeders/ProductsSeeder.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository: https://github.com/blacksmoke26/ims-backend
+
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Database.Seeders;
+
+public static class ProductsSeeder {
+  public static void SeedData(this EntityTypeBuilder<Product> entity) {
+  }
+}
