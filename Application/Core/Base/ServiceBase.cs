98d1d863dc6cdfddd6e5095b4ca421db241cd642
Junaid Atari <junaid.attari@invozone.dev>
2025-07-30 14:30:43 +0500

    docs: change header website

--- Application/Core/Base/ServiceBase.cs
+++ Application/Core/Base/ServiceBase.cs
@@ -1,6 +1,6 @@
 ﻿// Licensed to the end users under one or more agreements.
 // Copyright (c) 2025 Junaid Atari, and contributors
-// Website: https://github.com/blacksmoke26/
+// Repository: https://github.com/blacksmoke26/ims-backend
 
 namespace Application.Core.Base;
 
